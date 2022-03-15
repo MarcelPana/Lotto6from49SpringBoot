a0b13b6b5aecb64e880d99f8eaff3ae1d4f7c244
MarcelPana <79997464+MarcelPana@users.noreply.github.com>
2022-03-15 13:23:02 +0100

    add to database

---
+++ src/main/java/com/example/Lotto6from49/serviceImpl/AddToDataBase.java
@@ -0,0 +1,25 @@
+package com.example.Lotto6from49.serviceImpl;
+
+import com.example.Lotto6from49.entities.ExtractedNumbers;
+import com.example.Lotto6from49.entities.Lotto6From49;
+import com.example.Lotto6from49.repositories.ExtractedNumbersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddToDataBase {
+
+    ExtractedNumbersRepository extractedNumbersRepository;
+
+    @Autowired
+    public AddToDataBase(ExtractedNumbersRepository extractedNumbersRepository) {
+        this.extractedNumbersRepository = extractedNumbersRepository;
+    }
+
+    // Save lucky numbers in Database
+    public ExtractedNumbers addExNr(ExtractedNumbers extractedNumbers) {
+        System.out.println("Database " + extractedNumbers);
+        extractedNumbersRepository.save(extractedNumbers);
+        return extractedNumbers;
+    }
+}
