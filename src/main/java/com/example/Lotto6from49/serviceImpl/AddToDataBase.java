3fed2280728932c791a2b510de1d17ad187e4488
MarcelPana <79997464+MarcelPana@users.noreply.github.com>
2022-03-20 07:52:52 +0100

    service

--- src/main/java/com/example/Lotto6from49/serviceImpl/AddToDataBase.java
+++ src/main/java/com/example/Lotto6from49/serviceImpl/AddToDataBase.java
@@ -9,22 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddToDataBase {
 
-<<<<<<< HEAD
+
     private final ExtractedNumbersRepository extractedNumbersRepository;
-=======
-    ExtractedNumbersRepository extractedNumbersRepository;
->>>>>>> origin/validation
 
     @Autowired
     public AddToDataBase(ExtractedNumbersRepository extractedNumbersRepository) {
         this.extractedNumbersRepository = extractedNumbersRepository;
     }
 
-<<<<<<< HEAD
     // Save lucky numbers into Database
-=======
-    // Save lucky numbers in Database
->>>>>>> origin/validation
     public ExtractedNumbers addExNr(ExtractedNumbers extractedNumbers) {
         System.out.println("Database " + extractedNumbers);
         extractedNumbersRepository.save(extractedNumbers);
