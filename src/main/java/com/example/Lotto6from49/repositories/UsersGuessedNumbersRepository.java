17477eceb57b9881e744d822e5c4af4c384f983f
MarcelPana <79997464+MarcelPana@users.noreply.github.com>
2022-04-07 14:08:03 +0300

    com

---
+++ src/main/java/com/example/Lotto6from49/repositories/UsersGuessedNumbersRepository.java
@@ -0,0 +1,10 @@
+package com.example.Lotto6from49.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersGuessedNumbersRepository extends JpaRepository {
+
+
+}
---
+++ src/main/java/com/example/Lotto6from49/serviceImpl/AddUsersGuessedNrToDataBase.java
@@ -0,0 +1,26 @@
+package com.example.Lotto6from49.serviceImpl;
+
+import com.example.Lotto6from49.entities.ExtractedNumbers;
+import com.example.Lotto6from49.entities.UsersGuessedNumbers;
+import com.example.Lotto6from49.repositories.UsersGuessedNumbersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddUsersGuessedNrToDataBase {
+
+
+    private final UsersGuessedNumbersRepository usersGuessedNumbersRepository;
+
+    @Autowired
+    public AddUsersGuessedNrToDataBase(UsersGuessedNumbersRepository usersGuessedNumbersRepository) {
+        this.usersGuessedNumbersRepository = usersGuessedNumbersRepository;
+    }
+
+    // Save users guessed numbers into Database
+    public UsersGuessedNumbers addGuessedNr(UsersGuessedNumbers usersGuessedNumbers) {
+        System.out.println("Database " + usersGuessedNumbers);
+        usersGuessedNumbersRepository.save(usersGuessedNumbers);
+        return usersGuessedNumbers;
+    }
+}
